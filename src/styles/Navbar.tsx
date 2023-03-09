98c3cac846ee43506336c74b4a352fa32748eda5
Felipe Parra <fpr29.10@gmail.com>
2023-03-09 11:10:50 -0600

    Add Navbar, NavLogo and NavLinks styling components

---
+++ src/styles/Navbar.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Navbar = styled.div`
+  background: #fff;
+  // border-bottom: 1px solid #000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 1rem;
+  width: 100%;
+  height: 3rem;
+`;
+
+export const NavLogo = styled.div`
+  font-size: 1.5rem;
+  font-weight: 600;
+  width: 100%;
+`;
+
+export const NavLinks = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
