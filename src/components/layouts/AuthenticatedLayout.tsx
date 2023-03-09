fcd0429e07ce0f8cf2eb7056943a781a0d024cbe
Felipe Parra <fpr29.10@gmail.com>
2023-03-09 11:05:28 -0600

    Update AuthenticatedLayout to accept ReactNode as child prop

--- src/components/layouts/AuthenticatedLayout.tsx
+++ src/components/layouts/AuthenticatedLayout.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import Navbar from "../ui/Navbar";
 
-export default function AuthenticatedLayout({ children }: { children: any }) {
+export default function AuthenticatedLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <section className="flex flex-col justify-start items-start w-full h-full">
       <Navbar />
